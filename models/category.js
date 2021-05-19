61a56057c1bb8b533441bc5788efe19cb89c8573
sulman147 <msulman147@gmail.com>
2021-05-19 23:30:59 +0500

    Creating Products With category and sub category and making product pages backend api's

--- models/category.js
+++ models/category.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
+
 const categorySchema = new mongoose.Schema(
   {
     name: {
       type: String,
       trim: true,
       required: "Name is required",
-      minlength: [2, "Too Short"],
+      minlength: [2, "Too short"],
       maxlength: [32, "Too long"],
     },
     slug: {
---
+++ routes/product.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const router = express.Router();
+
+// middlewares
+const { authCheck, adminCheck } = require("../middlewares/auth");
+
+// controller
+const {
+  create,
+  listAll,
+  remove,
+  read,
+  update,
+  list,
+  productsCount,
+} = require("../controllers/product");
+
+// routes
+router.post("/product", authCheck, adminCheck, create);
+router.get("/products/total", productsCount);
+router.get("/products/:count", listAll); // products/100
+router.delete("/product/:slug", authCheck, adminCheck, remove);
+router.get("/product/:slug", read);
+router.put("/product/:slug", authCheck, adminCheck, update);
+
+router.post("/products", list);
+
+module.exports = router;
